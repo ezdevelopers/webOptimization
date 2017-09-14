6da591dc65c75b2a854a50d1a6076429e17704d4
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-09-14 10:16:47 +0100

    Optimise web fonts for page speed

--- js/analytics.js
+++ js/analytics.js
@@ -4,3 +4,14 @@
       // Optional TODO: replace with your Google Analytics profile ID.
       ga('create', 'UA-XXXX-Y');
       ga('send', 'pageview');
+
+var font = new FontFace("Open Sans", "url(//fonts.googleapis.com/css?family=Open+Sans:400,700)", {
+  style: 'normal', unicodeRange: 'U+000-5FF', weight: '400'
+});
+// don't wait for the render tree, initiate an immediate fetch!
+font.load().then(function() {
+  // apply the font (which may re-render text and cause a page reflow)
+  // after the font has finished downloading
+  document.fonts.add(font);
+  document.body.style.fontFamily = "Open Sans, serif"; 
+});
