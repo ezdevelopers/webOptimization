389e62814ea72a67791835e127e485c678bbd474
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-09-19 17:23:43 +0100

    Minify CSS and JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,30 @@ module.exports = function (grunt) {
                     dest: 'img/dist/'
                 }]
             }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src: ['style.css'],
+                    dest: 'css/build/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/build/perfmatters.min.js': ['js/perfmatters.js']
+                }
+            }
         }
-    });
 
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.registerTask('default', ['imagemin']);
+    grunt.registerTask('default', ['imagemin', 'cssmin','uglify']);
 
 };
